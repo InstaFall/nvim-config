aa7b6509c3df152f4c02637cd6b4d4bf2e01a379
Can Akıllı <gorkemcanakilli@hotmail.com>
2025-03-26 20:03:12 +0300

    Add Surround Keymap

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,14 @@
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    mappings = {
+      add = "şa",
+      delete = "şd",
+      find = "şf",
+      find_left = "şF",
+      highlight = "şh",
+      replace = "şr",
+      update_n_lines = "şn",
+    },
+  },
+}
