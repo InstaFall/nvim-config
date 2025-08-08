cc7358fdacb0f88e2f2738c4a89b222c206a75e1
Can Akıllı <gorkemcanakilli@hotmail.com>
2025-08-08 18:40:58 +0300

    Add Keymap For Delete Buffer (ALT+W)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,13 +5,13 @@
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
 
-vim.keymap.set("n", "<C-f>", "<C-f>zz", { desc = "Scroll down and center" })
-vim.keymap.set("n", "<C-b>", "<C-b>zz", { desc = "Scroll up and center" })
-
 vim.keymap.set("n", "<leader>rn", ":IncRename ")
 
+vim.keymap.set("n", "<A-w>", ":b#<bar>bd#<CR>",
+  { desc = "Close current buffer and switch to previous", silent = true, noremap = true })
+
 -- Bufferline ordinal tab switching with Alt+1..Alt+9
-for i = 1, 9 do
+for i = 1, 6 do
   vim.keymap.set("n", "<C-t>" .. i, function()
     require("bufferline").go_to_buffer(i, true)
   end, { desc = "Go to buffer tab " .. i, silent = true })
